0b83734e39dfc9892f1255d981db633b1521ed31
Maksym Schostak <109462224+mx111m@users.noreply.github.com>
2026-05-19 13:27:44 +0300

    Update Editor.java

--- src/domain/Editor.java
+++ src/domain/Editor.java
@@ -1,29 +1,35 @@
 package domain;
-public class Editor extends Artist {
-
-    public Editor(boolean electronicEditing, String[] skiils, String name, String jobTitle, int level, String dept) {
-        super(skiils, name, jobTitle, level, dept);
-        this.electronicEditing = electronicEditing;
-    }
-
     public Editor(boolean electronicEditing, String[] skiils) {
         super(skiils);
         this.electronicEditing = electronicEditing;
     }
 
+    /**
+     * Створює редактора.
+     *
+     * @param electronicEditing тип редагування
+     */
     public Editor(boolean electronicEditing) {
         super();
         this.electronicEditing = electronicEditing;
     }
     
+    /**
+     * Створює редактора за замовчуванням.
+     */
     public Editor() {
         super();
         this.electronicEditing = true;
     }
 
+    /**
+     * Повертає інформацію про редактора.
+     *
+     * @return інформація про редактора
+     */
     @Override
     public String toString() {
-        String s=super.toString()+"\nEditing preferences= "; 
+        String s=super.toString()+"\nEditing preferences= ";
         if (electronicEditing)
             s=s+"electronic";
         else
@@ -34,10 +40,20 @@ public class Editor extends Artist {
 
     private boolean electronicEditing;
 
+    /**
+     * Повертає тип редагування.
+     *
+     * @return true якщо електронне редагування
+     */
     public boolean getPreferences() {
         return electronicEditing;
     }
 
+    /**
+     * Встановлює тип редагування.
+     *
+     * @param electronic тип редагування
+     */
     public void setPreferences(boolean electronic) {
         this.electronicEditing=electronic;
     }
